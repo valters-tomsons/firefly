3d5eab7fc477db799c749315f8787d1711b0e666
Faith <infinity1188@yahoo.com>
2018-04-28 17:18:14 +0300

    Implement OpCodeTable<> dictionary and execute the first instruction

--- firefly/Cpu/OpCodeExecutor.cs
+++ firefly/Cpu/OpCodeExecutor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using firefly.Domain;
 
 namespace firefly.Cpu
@@ -8,19 +9,27 @@ namespace firefly.Cpu
     sealed class OpCodeExecutor
     {
         private CPU CPU;
+        private Dictionary<UInt32, Action<Instruction>> OpCodeTable;
 
         public OpCodeExecutor(CPU cpu)
         {
+            Init_OpCodeTable();
             CPU = cpu;
+        }
+
+        private void Init_OpCodeTable()
+        {
+            OpCodeTable = new Dictionary<UInt32, Action<Instruction>>
+            {
+                { 0b001111, LUI },
+                { 0b001101, ORI },
+            };
         }
 
         public void Execute(Instruction i)
         {
-            Console.WriteLine($"{i.Func:X}");
-            Console.WriteLine($"{i.Index:X}");
-            Console.WriteLine($"{i.Imm:X}");
-
-            throw new Exceptions.UnhandledInstructionException(i);
+            //Console.WriteLine($"{i.Func:X}");
+            OpCodeTable[i.Func](i);
         }
 
         //Load Upper Immediate
@@ -28,13 +37,12 @@ namespace firefly.Cpu
         {
             //set low 16bits to 0
             UInt32 v = i.Imm << 16;
-
             CPU.R[i.Index] = v;
         }
 
         private void ORI(Instruction i)
         {
-
+            UInt32 v;
         }
     }
 }
--- firefly/Domain/Instruction.cs
+++ firefly/Domain/Instruction.cs
@@ -12,8 +12,13 @@ namespace firefly.Domain
 
         public Instruction(UInt32 Address)
         {
+            //31:26 bits of instruction
             Func = Address >> 26;
+
+            //Register index in bits 20:16
             Index = (Address >> 16) & 0xf1;
+
+            //Immedaite value in bits 16:0
             Imm = Address & 0xffff;
         }
     }
