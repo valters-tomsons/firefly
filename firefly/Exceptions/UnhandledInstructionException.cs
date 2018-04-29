366b2bb9b2ae917ae7425ad9ed353bbbad9a9a90
Faith <infinity1188@yahoo.com>
2018-04-29 05:26:31 +0300

    Display missing instruction op when UnhandledInstructionException is thrown

--- firefly/Exceptions/UnhandledInstructionException.cs
+++ firefly/Exceptions/UnhandledInstructionException.cs
@@ -5,9 +5,14 @@ namespace firefly.Exceptions
 {
     class UnhandledInstructionException : Exception
     {
-        public UnhandledInstructionException(Instruction Address)
+        public UnhandledInstructionException(Instruction i) : base(Message(i))
         {
 
+        }
+
+        private new static string Message(Instruction i)
+        {
+            return $"Unhandled instruction 0x{i.Func:X} tried to execute.";
         }
     }
 }
